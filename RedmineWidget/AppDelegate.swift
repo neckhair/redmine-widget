a2972ffb0c82c4a3322d70384a560d95a962e7ad
Philippe Hässig <phil@nine.ch>
2016-01-08 14:15:20 +0100

    settings window works

--- RedmineWidget/AppDelegate.swift
+++ RedmineWidget/AppDelegate.swift
@@ -19,13 +19,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var saveButton: NSButton!
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let defaults = NSUserDefaults.standardUserDefaults()
+
+        if let url = defaults.stringForKey("RedmineURL") {
+            urlTextField.stringValue = url
+        }
+        if let token = defaults.stringForKey("ApiToken") {
+            tokenTextField.stringValue = token
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    @IBAction func saveSettings(sender: NSButton) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setValue(urlTextField.stringValue, forKey: "RedmineURL")
+        defaults.setValue(tokenTextField.stringValue, forKey: "ApiToken")
+        NSApplication.sharedApplication().terminate(self)
+    }
 
+    @IBAction func quitApplication(sender: AnyObject) {
+        NSApplication.sharedApplication().terminate(self)
+    }
 }
 
